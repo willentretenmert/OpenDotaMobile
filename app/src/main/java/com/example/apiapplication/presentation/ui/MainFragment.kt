3e6ce0f73575bba4646b860765b51fa918cf3551
EugeneKzxc <minengisob@gmail.com>
2023-03-27 00:23:21 +0300

    mvvm

--- app/src/main/java/com/example/apiapplication/presentation/ui/MainFragment.kt
+++ app/src/main/java/com/example/apiapplication/presentation/ui/MainFragment.kt
@@ -1,28 +1,24 @@
 package com.example.apiapplication.presentation.ui
 
 import android.os.Bundle
-import android.os.StrictMode
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.apiapplication.R
-import com.example.apiapplication.data.Hero
-import com.example.apiapplication.data.PlayerStats
 import com.example.apiapplication.databinding.FragmentMainBinding
-import com.google.gson.Gson
-import java.net.URL
+import com.example.apiapplication.presentation.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
 
-    private val gson = Gson()
-    //private val _binding : FragmentMainBinding? = null
-    //private val binding get() = _binding!!
     private lateinit var binding: FragmentMainBinding
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
 
     override fun onCreateView(
@@ -37,49 +33,42 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel.fetchHeroes()
+
         binding.button.setOnClickListener {
             if (binding.editText.text.isNotBlank()) {
-                val id = binding.editText.text
-                val policy = StrictMode.ThreadPolicy.Builder()
-                    .permitAll()
-                    .build()
-                StrictMode.setThreadPolicy(policy)
-                // 1068042013 - my id
-                // 275690206 - eugenesub id
-                // old response val, don't use until multiple usages created
-                //val response = URL("https://api.opendota.com/api/heroes").readText()
+                val id = binding.editText.text.toString()
+                viewModel.fetchPlayerStats(id)
 
-                val data = gson.fromJson(
-                    URL("https://api.opendota.com/api/heroes").readText(),
-                    Array<Hero>::class.java
-                )
-                val data2 = gson.fromJson(
-                    URL("https://api.opendota.com/api/players/$id/heroes").readText(),
-                    Array<PlayerStats>::class.java
-                )
-
-                for (i in 0..2) {
-                    getHeroNameByIndex(data, data2, i)
-                    val appendix: String = "\nMP: " + data2[i].games.toString() + "\n" +
-                            "WR: " + String.format("%.0f", data2[i].win.toDouble() / data2[i].games.toDouble() * 100) + "%"
-                    when(i) {
-                        0 -> binding.textView.append(appendix)
-                        1 -> binding.textView2.append(appendix)
-                        2 -> binding.textView3.append(appendix)
+                viewModel.heroes.observe(viewLifecycleOwner) { data ->
+                    viewModel.playerStats.observe(viewLifecycleOwner) { data2 ->
+                        for (i in 0..2) {
+                            val heroName = viewModel.getHeroNameByIndex(data, data2, i)
+                            val appendix: String =
+                                "\nMP: " + data2[i].games.toString() + "\n" +
+                                        "WR: " + String.format(
+                                    "%.0f",
+                                    data2[i].win.toDouble() / data2[i].games.toDouble() * 100
+                                ) + "%"
+                            when (i) {
+                                0 -> {
+                                    binding.textView.text = heroName
+                                    binding.textView.append(appendix)
+                                }
+                                1 -> {
+                                    binding.textView2.text = heroName
+                                    binding.textView2.append(appendix)
+                                }
+                                2 -> {
+                                    binding.textView3.text = heroName
+                                    binding.textView3.append(appendix)
+                                }
+                            }
+                        }
                     }
                 }
-            } else Toast.makeText(requireContext(), getString(R.string.tv_err), Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun getHeroNameByIndex(data: Array<Hero>, data2: Array<PlayerStats>, i: Int) {
-        data.forEach { v ->
-            if (v.id == data2[i].hero_id.toInt()) {
-                when(i) {
-                    0 -> binding.textView.text = v.localized_name
-                    1 -> binding.textView2.text = v.localized_name
-                    2 -> binding.textView3.text = v.localized_name
-                }
+            } else {
+                Toast.makeText(requireContext(), getString(R.string.tv_err), Toast.LENGTH_SHORT).show()
             }
         }
     }
--- app/src/main/java/com/example/apiapplication/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/apiapplication/presentation/viewmodel/MainViewModel.kt
@@ -1,5 +1,46 @@
 package com.example.apiapplication.presentation.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.apiapplication.data.Hero
+import com.example.apiapplication.data.PlayerStats
+import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.net.URL
 
+class MainViewModel : ViewModel() {
+
+    private val gson = Gson()
+    private val _heroes = MutableLiveData<List<Hero>>()
+    val heroes: LiveData<List<Hero>> = _heroes
+
+    private val _playerStats = MutableLiveData<List<PlayerStats>>()
+    val playerStats: LiveData<List<PlayerStats>> = _playerStats
+
+    fun fetchHeroes() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val data = gson.fromJson(
+                URL("https://api.opendota.com/api/heroes").readText(),
+                Array<Hero>::class.java
+            )
+            _heroes.postValue(data.toList())
+        }
+    }
+
+    fun fetchPlayerStats(id: CharSequence) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val data2 = gson.fromJson(
+                URL("https://api.opendota.com/api/players/$id/heroes").readText(),
+                Array<PlayerStats>::class.java
+            )
+            _playerStats.postValue(data2.toList())
+        }
+    }
+
+    fun getHeroNameByIndex(data: List<Hero>, data2: List<PlayerStats>, i: Int): String {
+        return data.firstOrNull { v -> v.id == data2[i].hero_id.toInt() }?.localized_name ?: ""
+    }
 }

8489d8061ba32bf5b9d2334d6ff11d922ba48666
EugeneKzxc <minengisob@gmail.com>
2023-05-09 23:27:10 +0300

    7.33s firebase auth

--- app/src/main/java/com/example/apiapplication/presentation/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/apiapplication/presentation/ui/fragments/LoginFragment.kt
@@ -1,19 +1,27 @@
 package com.example.apiapplication.presentation.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import android.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.apiapplication.R
 import com.example.apiapplication.databinding.FragmentLoginBinding
+import com.example.apiapplication.presentation.viewmodel.LoginViewModel
+import com.example.apiapplication.presentation.viewmodel.MainViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : Fragment() {
 
     private lateinit var binding : FragmentLoginBinding
+    private val viewModel : LoginViewModel by lazy { ViewModelProvider(this)[LoginViewModel::class.java] }
+    val mAuth = FirebaseAuth.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,11 +41,14 @@ class LoginFragment : Fragment() {
             if (binding.editTextLogin.text.isNotBlank() and binding.editTextPassword.text.isNotBlank()) {
                 val login = binding.editTextLogin.text.toString()
                 val password = binding.editTextPassword.text.toString()
-                if (login == "zxc" && password == "123") {
-                    findNavController().navigate(R.id.action_loginFragment_to_searchFragment)
-                    val bottomNavigation =
-                        activity?.findViewById<BottomNavigationView>(R.id.navigation)
-                    bottomNavigation?.visibility = View.VISIBLE
+                viewModel.getAuth(login, password) { loginResult ->
+                    if (loginResult)
+                    {
+                        findNavController().navigate(R.id.action_loginFragment_to_searchFragment)
+                            val bottomNavigation = activity?.findViewById<BottomNavigationView>(R.id.navigation)
+                            bottomNavigation?.visibility = View.VISIBLE
+                    }
+                    else Toast.makeText(context, "Authentication failed.", Toast.LENGTH_SHORT).show()
                 }
             }
         }
--- app/src/main/java/com/example/apiapplication/presentation/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/apiapplication/presentation/viewmodel/LoginViewModel.kt
@@ -1,9 +1,17 @@
 package com.example.apiapplication.presentation.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.navigation.fragment.findNavController
-import com.example.apiapplication.R
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginViewModel : ViewModel() {
+    private val auth = FirebaseAuth.getInstance()
 
+    fun getAuth(login: String, password: String, callback: (Boolean) -> Unit) {
+        auth.signInWithEmailAndPassword(login, password).addOnCompleteListener { task ->
+                val loginResult = if (task.isSuccessful) task.isSuccessful
+                else false
+                callback(loginResult)
+            }
+    }
 }

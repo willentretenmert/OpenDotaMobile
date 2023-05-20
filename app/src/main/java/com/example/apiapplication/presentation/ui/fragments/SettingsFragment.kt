a9fb2dcc75cb4f2902d3b931a80484e204ca6e54
EugeneKzxc <minengisob@gmail.com>
2023-05-20 19:58:05 +0300

    8.0

--- app/src/main/java/com/example/apiapplication/presentation/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/apiapplication/presentation/ui/fragments/SettingsFragment.kt
@@ -1,17 +1,21 @@
 package com.example.apiapplication.presentation.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.apiapplication.R
 import com.example.apiapplication.databinding.FragmentSettingsBinding
 import com.example.apiapplication.presentation.ui.activity.MainActivity
 import com.example.apiapplication.presentation.viewmodel.SettingsViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
 
 class SettingsFragment : Fragment(){
 
@@ -26,7 +30,17 @@ class SettingsFragment : Fragment(){
         binding = FragmentSettingsBinding.inflate(layoutInflater, container, false)
         val bottomNavigation = activity?.findViewById<BottomNavigationView>(R.id.navigation)
         bottomNavigation?.visibility = View.VISIBLE
-        binding.username.setText(MainActivity.User.auth.currentUser!!.email.toString())
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.fetchFirebaseProfile(MainActivity.User.auth.currentUser!!.email.toString())
+        }
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.firebaseProfile.collect() { profile ->
+                val nickname = profile?._name
+                binding.email.setText(MainActivity.User.auth.currentUser!!.email.toString())
+                binding.editNickname.setText(nickname)
+            }
+        }
+
         setEventListener()
         return binding.root
     }
@@ -37,6 +51,13 @@ class SettingsFragment : Fragment(){
             findNavController().apply {
                 navigate(R.id.action_settingsFragment_to_loginFragment)
                 popBackStack(R.id.settingsFragment, true)
+            }
+        }
+        binding.editProfileButton.setOnClickListener {
+            if (binding.editNickname.text.isNotBlank()) {
+                viewModel.updateNickname(MainActivity.User.auth.currentUser!!.email.toString(),binding.editNickname.text.toString()) { success ->
+                    Log.d("zxc", "good")
+                }
             }
         }
     }
